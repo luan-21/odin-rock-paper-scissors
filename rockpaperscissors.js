7290fa1998bfeacbeebfc97da2dbdbbe4f2be747
luan-21 <5956687+luan-21@users.noreply.github.com>
2026-04-20 15:07:04 -0700

    pseudocode

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,39 @@
+/*  
+    function get computer choice
+        set choices to ["rock", "paper", "scissors"]
+        pick a random value from choices
+        return the value
+
+    function get human choice
+        ask user for input
+        store input as value
+        return value
+
+    set humanScore to 0
+    set computerScore to 0
+
+    function play round (humanChoice, computerChoice)
+        if humanChoice equals computerChoice
+        return "YOU TIED"
+        if humanChoice is rock and computerChoice is scissors
+        return "YOU WIN"
+        if humanChoice is paper and computerChoice is rock
+        return "YOU WIN"
+        if humanChoice is scissors and computerChoice is paper
+        return "YOU WIN"
+        else return "YOU LOST"
+
+    function  scores (one,two)
+        if play round equal to YOU WIN
+        increase humanScore by 1
+
+        if play round equal to YOU LOST
+        increase computerScore by 1
+
+    display playround
+    display humanScore
+    display computerScore
+
+*/
+
 console.log("Hello World");
